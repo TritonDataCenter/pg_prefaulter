dba9efe5d7d3f2ef8c97ffd932b0ba7ed7095a98
Sean Chittenden <sean@chittenden.org>
2017-09-01 22:11:31 -0700

    Improve the LSN type to include Segment() and WALFileName() helper methods.

--- lsn/lsn.go
+++ lsn/lsn.go
@@ -2,44 +2,85 @@ package lsn
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 	"strings"
 
+	"github.com/alecthomas/units"
 	"github.com/pkg/errors"
 )
 
 type LSN uint64
 
+const (
+	// Value representing an invalid LSN (used in error conditions)
+	InvalidLSN = LSN(math.MaxUint64)
+
+	// WALPageSize defaults to 8KB
+	WALPageSize = 8 * units.KiB
+
+	// WALSegmentSize defaults to 16MB
+	WALSegmentSize              = 16 * units.MiB
+	WALSegmentsPerXLogId uint64 = 0x100000000 / uint64(WALSegmentSize)
+)
+
+// Parse returns a parsed LSN
 func Parse(in string) (LSN, error) {
 	parts := strings.Split(in, "/")
 	if len(parts) != 2 {
-		return 0, fmt.Errorf("invalid LSN: %q", in)
+		return InvalidLSN, fmt.Errorf("invalid LSN: %q", in)
 	}
 
 	id, err := strconv.ParseUint(parts[0], 16, 32)
 	if err != nil {
-		return 0, errors.Wrap(err, "unable to decode the segment ID")
+		return InvalidLSN, errors.Wrap(err, "unable to decode the segment ID")
 	}
 
 	offset, err := strconv.ParseUint(parts[1], 16, 32)
 	if err != nil {
-		return 0, errors.Wrap(err, "unable to decode the segment ID")
+		return InvalidLSN, errors.Wrap(err, "unable to decode the segment ID")
 	}
 
 	return LSN(uint64(id)<<32 | offset), nil
 }
 
+// ID returns the numeric ID of the WAL.
 func (lsn LSN) ID() uint32 {
 	return uint32(lsn >> 32)
 }
 
+// Offset returns the byte offset inside of a WAL segment.
 func (lsn LSN) Offset() uint32 {
 	return uint32(lsn)
 }
 
+// Segment returns the Segment number of the LSN.
+func (lsn LSN) Segment() uint64 {
+	return uint64(lsn) / uint64(WALSegmentSize)
+}
+
+// String returns the string representation of an LSN.
 func (lsn LSN) String() string {
 	var id, offset uint32
 	id = uint32(lsn >> 32)
 	offset = uint32(lsn)
 	return fmt.Sprintf("%X/%X", id, offset)
+}
+
+// WALFileName returns the name of a WAL's filename.  The timeline number is
+// optional.  If the timeline is not specified, default to a timelineID of 1.
+func (lsn LSN) WALFileName(timelineID ...uint32) string {
+	var tid uint32
+	switch len(timelineID) {
+	case 0:
+		tid = 1
+	case 1:
+		tid = timelineID[0]
+	default:
+		panic("only one timelineID supported")
+	}
+
+	return fmt.Sprintf("%08X%08X%08X", tid,
+		lsn.Segment()/WALSegmentsPerXLogId,
+		lsn.Segment()%WALSegmentsPerXLogId)
 }
--- lsn/lsn_test.go
+++ lsn/lsn_test.go
@@ -7,27 +7,69 @@ import (
 	"github.com/kylelemons/godebug/pretty"
 )
 
+// Precompute the expected results from constants
+func TestConstants(t *testing.T) {
+	if diff := pretty.Compare(lsn.WALPageSize, 8192); diff != "" {
+		t.Fatalf("WALPageSize diff: (-got +want)\n%s", diff)
+	}
+
+	if diff := pretty.Compare(lsn.WALSegmentSize, 16777216); diff != "" {
+		t.Fatalf("WALSegmentSize diff: (-got +want)\n%s", diff)
+	}
+
+	if diff := pretty.Compare(lsn.WALSegmentsPerXLogId, 256); diff != "" {
+		t.Fatalf("WALSegmentsPerXLogId diff: (-got +want)\n%s", diff)
+	}
+}
+
 func TestType(t *testing.T) {
 	tests := []struct {
-		num    uint64
-		in     string
-		out    string
-		id     uint32
-		offset uint32
+		in       string
+		out      string
+		num      uint64
+		timeline uint32
+		filename string
+		id       uint32
+		offset   uint32
+		segment  uint32
 	}{
 		{
-			num:    22077776,
-			in:     "0/150E150",
-			out:    "0/150E150",
-			id:     0,
-			offset: 22077776,
+			in:       "0/150E150",
+			out:      "0/150E150",
+			num:      22077776,
+			filename: "000000010000000000000001",
+			id:       0,
+			offset:   22077776,
+			segment:  1,
 		},
 		{
-			num:    22194624,
-			in:     "00/152A9C0",
-			out:    "0/152A9C0",
-			id:     0,
-			offset: 22194624,
+			in:       "00/152A9C0",
+			out:      "0/152A9C0",
+			num:      22194624,
+			timeline: 1,
+			filename: "000000010000000000000001",
+			id:       0,
+			offset:   22194624,
+			segment:  1,
+		},
+		{
+			in:       "00/272E4558",
+			out:      "0/272E4558",
+			num:      657343832,
+			filename: "000000010000000000000027",
+			id:       0,
+			offset:   657343832,
+			segment:  39,
+		},
+		{
+			in:       "FF/362E4558",
+			out:      "FF/362E4558",
+			num:      1096125662552,
+			timeline: 0xff,
+			filename: "000000FF000000FF00000036",
+			id:       255,
+			offset:   909002072,
+			segment:  65334,
 		},
 	}
 
@@ -45,16 +87,32 @@ func TestType(t *testing.T) {
 				st.Fatalf("%d: LSN diff: (-got +want)\n%s", n, diff)
 			}
 
-			if diff := pretty.Compare(test.out, l.String()); diff != "" {
-				st.Fatalf("%d: String() diff: (-got +want)\n%s", n, diff)
-			}
-
 			if diff := pretty.Compare(test.id, l.ID()); diff != "" {
 				st.Fatalf("%d: ID diff: (-got +want)\n%s", n, diff)
 			}
 
 			if diff := pretty.Compare(test.offset, l.Offset()); diff != "" {
 				st.Fatalf("%d: Offset diff: (-got +want)\n%s", n, diff)
+			}
+
+			if diff := pretty.Compare(test.segment, l.Segment()); diff != "" {
+				st.Fatalf("%d: Segment diff: (-got +want)\n%s", n, diff)
+			}
+
+			if diff := pretty.Compare(test.out, l.String()); diff != "" {
+				st.Fatalf("%d: String() diff: (-got +want)\n%s", n, diff)
+			}
+
+			// Test optional argument
+			switch test.timeline {
+			case 0:
+				if diff := pretty.Compare(test.filename, l.WALFileName()); diff != "" {
+					st.Fatalf("%d: WALFileName diff: (-got +want)\n%s", n, diff)
+				}
+			default:
+				if diff := pretty.Compare(test.filename, l.WALFileName(test.timeline)); diff != "" {
+					st.Fatalf("%d: WALFileName diff: (-got +want)\n%s", n, diff)
+				}
 			}
 		})
 	}
